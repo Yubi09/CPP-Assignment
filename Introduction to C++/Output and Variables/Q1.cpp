6de060d8a54ce52f9b18ddd8029c0655a4bb71cd
Yubi09 <ydas6872@gmail.com>
2025-01-05 18:15:28 +0530

    Output and Variables Assignment

---
+++ Introduction to C++/Output and Variables/Q1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "Two ways to output 'Physics' and 'Wallah' in different line in C++ are: " << endl;
+  // Using endl
+  cout << "Physics" << endl;
+  cout << "Wallah" << endl;
+
+  // Using '\n'
+  cout << "Physics\n";
+  cout << "Wallah\n";
+  return 0;
+}
---
+++ Introduction to C++/Output and Variables/Q5.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << 2 * 3.14 * 10 << endl;
+  return 0;
+}
